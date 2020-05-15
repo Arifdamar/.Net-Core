23e1a14ddd766bef360cb580ba291432500ff20e
Arif Damar <38383027+Arifdamar@users.noreply.github.com>
2020-05-15 22:52:08 +0300

    Custom exception class

--- consoleApp/Program.cs
+++ consoleApp/Program.cs
@@ -3,84 +3,43 @@ using System.Linq;
 
 namespace consoleApp
 {
+
+    class LoginException: Exception
+    {
+        public LoginException(string message):base(message)
+        {
+        }
+
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
 
-            // "1","2","5a","10b","abc","10","50"
-            // 1- Liste içerisindeki sayısal değerleri bulunuz.
-
-            //    var liste = new List<string>()
-            //    {
-            //        "1","2","5a","10b","abc","10","50"
-            //    };
-
-            //    foreach (var item in liste)
-            //    {
-            //        try
-            //        {                       
-            //             int a = int.Parse(item);
-            //             Console.WriteLine(a);
-            //        }
-            //        catch (Exception)
-            //        {
-            //            continue;
-            //        }
-            //    }
-
-            // 2- Kullanıcı 'q' değerini girmedikçe aldığınız her değerin
-            // sayısal olup olmadığını kontrol edin aksi halde hata mesajı veriniz.
-
-            //    while(true)
-            //    {
-            //        Console.Write("sayı: ");
-            //        string input = Console.ReadLine();
-
-            //        if (input == "q")
-            //             break;
-
-            //         try
-            //         {
-            //             int sayi = int.Parse(input);
-            //             Console.WriteLine("girdiğiniz sayı: {0}",sayi);
-            //         }
-            //         catch (Exception)
-            //         {
-            //             Console.WriteLine("geçersiz sayı");
-            //             continue;
-            //         }             
-
-            //    }
-
-            // 3- Girilen parola içinde türkçe karakter araması yapınız.  
-
-            Console.Write("parola: ");
-            string parola = Console.ReadLine();
-
             try
             {
-                CheckPassword(parola);
+                Login("arifdamar", "12345678");
+                System.Console.WriteLine("Login successful");
             }
-            catch (Exception ex)
+            catch (LoginException ex)
             {
-                Console.Write(ex.Message);
+                System.Console.WriteLine(ex.Message);
+                throw;
             }
+
         }
 
-        static void CheckPassword(string parola)
+        static void Login(string username, string password)
         {
-            string turkce_karakterler = "ğ,Ğ,ç,Ç,ş,Ş,ü,Ü,ö,Ö,ı,İ";
-
-            foreach (var karakter in parola)
+            if(username.Contains(" "))
             {
-                if (turkce_karakterler.IndexOf(karakter) > -1)
-                    throw new Exception("Parola türkçe karakter içeremez");
+                throw new LoginException("Username cannot contain space character");
             }
-            Console.Write("geçerli parola");
+            if(password.Length<8)
+            {
+                throw new LoginException("Password must be at least 8 characters");
+            }
         }
-
-
     }
-
 }

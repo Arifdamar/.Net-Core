6447aa313bc16785d1ee82a0be515c36e6ea585c
Arif Damar <38383027+Arifdamar@users.noreply.github.com>
2020-05-15 20:10:24 +0300

    Exception handling

--- consoleApp/Program.cs
+++ consoleApp/Program.cs
@@ -1,38 +1,60 @@
 ﻿using System;
 
-namespace consoleApp {
+namespace consoleApp
+{
 
-    class Product {
+    class Product
+    {
         public string Name { get; set; }
     }
 
-    class Program {
-        static void Main (string[] args) {
+    class Program
+    {
+        static void Main(string[] args)
+        {
             // Exception
 
             // System.FormatException
             // System.DivideByZeroException
-
-            Console.Write ("Number1: ");
-            int number1 = int.Parse (Console.ReadLine ());
-
-            Console.Write ("Number2: ");
-            int number2 = int.Parse (Console.ReadLine ());
-
-            var result = number1 / number2;
-
             // System.IndexOutOfRangeException
-            int[] numbers = new int[2];
-
-            numbers[2] = 10;
-            
-
-            Product p = null;
-
             // System.NullReferenceException            
-            Console.WriteLine (p.Name);
+
 
             // Exception handling
+
+            try
+            {
+
+                Console.Write("a: ");
+                int a = int.Parse(Console.ReadLine());
+
+                Console.Write("b: ");
+                int b = int.Parse(Console.ReadLine());
+
+                var result = a / b;
+
+                Console.WriteLine("{0} / {1} = {2}", a, b, result);
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Second number cannot be 0");
+                System.Console.WriteLine(ex.Message);
+            }
+            catch(FormatException ex)
+            {
+                Console.WriteLine("Invalid format");
+                System.Console.WriteLine(ex.Message);
+            }
+            catch(Exception ex)
+            {
+                System.Console.WriteLine("An error occured");
+                System.Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                System.Console.WriteLine("Finally works!");
+            }
+
 
         }
     }

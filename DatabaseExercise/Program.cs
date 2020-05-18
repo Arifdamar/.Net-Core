3ddec576360102be85a247b3a339875aca3cf6d7
Arif Damar <38383027+Arifdamar@users.noreply.github.com>
2020-05-18 12:22:35 +0300

    MySql connection

--- DatabaseExercise/Program.cs
+++ DatabaseExercise/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Data.SqlClient;
+using MySql.Data.MySqlClient;
 
 namespace DatabaseExercise
 {
@@ -7,18 +8,20 @@ namespace DatabaseExercise
     {
         static void Main(string[] args)
         {
-            
-            GetSqlConnection();
+
+            //GetSqlConnection();
+            GetMySqlConnection();
 
         }
 
-        static void GetSqlConnection()
+        static void GetMySqlConnection()
         {
+
             string connectionString = @"Data Source=.\SQLEXPRESS;Initial Catalog=Northwind;Integrated Security=SSPI;";
 
             // driver, provider
 
-            using(var connection = new SqlConnection(connectionString))
+            using (var connection = new SqlConnection(connectionString))
             {
                 try
                 {
@@ -33,10 +36,38 @@ namespace DatabaseExercise
                 {
                     connection.Close();
                 }
-                
+
             }
-            
+
 
         }
+
+        static void GetSqlConnection()
+        {
+            string connectionString = @"server=localhost;port=3306;database=northwind;user=root;password=123456789;";
+
+            // driver, provider
+
+            using (var connection = new MySqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    System.Console.WriteLine("Connection Successful");
+                }
+                catch (Exception e)
+                {
+                    System.Console.WriteLine(e.Message);
+                }
+                finally
+                {
+                    connection.Close();
+                }
+
+            }
+
+
+        }
+
     }
 }

3e12fdc656424b5704d4d468043cfd5699167ba0
Arif Damar <38383027+Arifdamar@users.noreply.github.com>
2020-05-18 12:07:38 +0300

    Database connection

---
+++ DatabaseExercise/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace DatabaseExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            GetSqlConnection();
+
+        }
+
+        static void GetSqlConnection()
+        {
+            string connectionString = @"Data Source=.\SQLEXPRESS;Initial Catalog=Northwind;Integrated Security=SSPI;";
+
+            // driver, provider
+
+            using(var connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    System.Console.WriteLine("Connection Successful");
+                }
+                catch (Exception e)
+                {
+                    System.Console.WriteLine(e.Message);
+                }
+                finally
+                {
+                    connection.Close();
+                }
+                
+            }
+            
+
+        }
+    }
+}

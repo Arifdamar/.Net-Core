d991fc58dbd1e0b3c5531181c5e5a47b398be924
Arif Damar <38383027+Arifdamar@users.noreply.github.com>
2020-05-19 15:17:36 +0300

    Adding entity classes

--- ORM-Entity Framework Core/Program.cs
+++ ORM-Entity Framework Core/Program.cs
@@ -1,7 +1,25 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace ORM_Entity_Framework_Core
 {
+
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [MaxLength(100)]
+        [Required]
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
     class Program
     {
         static void Main(string[] args)

c80ce987c22eae85f0073ac838d5229924088bbf
Arif Damar <38383027+Arifdamar@users.noreply.github.com>
2020-05-19 16:06:29 +0300

    Adding data to database and logging sql commands

--- ORM-Entity Framework Core/Program.cs
+++ ORM-Entity Framework Core/Program.cs
@@ -1,17 +1,24 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace ORM_Entity_Framework_Core
 {
-    public class ShopContext: DbContext
+    public class ShopContext : DbContext
     {
-        public DbSet<Product> Products {get; set;}
+        public DbSet<Product> Products { get; set; }
         public DbSet<Category> Categories { get; set; }
+
+        public static readonly ILoggerFactory MyLoggerFactory
+        = LoggerFactory.Create(builder => { builder.AddConsole(); });
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlite("Data Source=shop.db");
+            optionsBuilder
+            .UseLoggerFactory(MyLoggerFactory)
+            .UseSqlite("Data Source=shop.db");
         }
     }
 
@@ -35,7 +42,47 @@ namespace ORM_Entity_Framework_Core
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            AddProduct();
         }
+
+        static void AddProducts()
+        {
+            using (var db = new ShopContext())
+            {
+                var products = new List<Product>
+                {
+                    new Product { Name = "Samsung Galaxy S8", Price = 4000 },
+                    new Product { Name = "Samsung Galaxy S8+", Price = 4500 },
+                    new Product { Name = "Samsung Galaxy S9", Price = 5000 },
+                    new Product { Name = "Samsung Galaxy S9+", Price = 5500 },
+                    new Product { Name = "Samsung Galaxy S10", Price = 6000 },
+                    new Product { Name = "Samsung Galaxy S10+", Price = 6500 }
+                };
+
+                db.Products.AddRange(products);
+
+                db.SaveChanges();
+
+                System.Console.WriteLine("Datas added to database");
+
+            }
+
+        }
+
+        static void AddProduct()
+        {
+            using (var db = new ShopContext())
+            {
+                var p = new Product { Name = "Samsung Galaxy S8", Price = 4000 };
+
+                db.Products.Add(p);
+
+                db.SaveChanges();
+
+                System.Console.WriteLine("Data added to database");
+
+            }
+        }
+
     }
 }

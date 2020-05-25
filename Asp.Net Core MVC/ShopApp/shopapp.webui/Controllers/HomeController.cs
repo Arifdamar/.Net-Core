81b570480bbf5dad34858ddee67d97c6c39b1b34
Arif Damar <38383027+Arifdamar@users.noreply.github.com>
2020-05-25 12:34:45 +0300

    Using dynamic data

--- Asp.Net Core MVC/ShopApp/shopapp.webui/Controllers/HomeController.cs
+++ Asp.Net Core MVC/ShopApp/shopapp.webui/Controllers/HomeController.cs
@@ -1,16 +1,20 @@
+using System;
 using Microsoft.AspNetCore.Mvc;
 
 namespace shopapp.webui.Controllers
 {
-    public class HomeController: Controller
+    public class HomeController : Controller
     {
 
         // localhost:5000/home/index
         public IActionResult Index()
         {
+            ViewBag.Greeting = DateTime.Now.Hour < 12 ? "Good Morning" : "Have a good day";
+            ViewBag.UserName = "Arif";
+
             return View();
         }
-        
+
         // localhost:5000/home/about
         public IActionResult About()
         {

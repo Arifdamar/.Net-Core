b27c7c901d149a435a8cd65a9aaeee21267133b6
Arif Damar <38383027+Arifdamar@users.noreply.github.com>
2020-06-23 13:49:25 +0300

    Creating and adding neccessary referances

---
+++ IdentityExample/IdentityExample/Context/IdentityContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityExample.Context
+{
+    public class IdentityContext : IdentityDbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("database=(localdb)\\MSSQLLocalDB;database=Identity;integrated security=true;");
+        }
+    }
+}

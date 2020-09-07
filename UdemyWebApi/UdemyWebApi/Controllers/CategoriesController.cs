57fc147c83d8fd22eea86610d3a342a7010235ea
Arif Damar <38383027+Arifdamar@users.noreply.github.com>
2020-09-07 15:00:29 +0300

    basic CRUD operations

--- UdemyWebApi/UdemyWebApi/Controllers/CategoriesController.cs
+++ UdemyWebApi/UdemyWebApi/Controllers/CategoriesController.cs
@@ -20,5 +20,64 @@ namespace UdemyWebApi.Controllers
 
             return Ok(context.Categories.ToList());
         }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            using var context = new UdemyWebApiContext();
+            var category = context.Categories.Find(id);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(category);
+        }
+
+        [HttpPut]
+        public IActionResult Update(Category category)
+        {
+            using var context = new UdemyWebApiContext();
+            var categoryToUpdate = context.Categories.Find(category.Id);
+
+            if (categoryToUpdate == null)
+            {
+                return NotFound();
+            }
+
+            categoryToUpdate.Name = category.Name;
+            context.Update(categoryToUpdate);
+            context.SaveChanges();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            using var context = new UdemyWebApiContext();
+            var categoryToDelete = context.Find<Category>(id);
+
+            if (categoryToDelete == null)
+            {
+                return NotFound();
+            }
+
+            context.Remove(categoryToDelete);
+            context.SaveChanges();
+
+            return NoContent();
+        }
+
+        [HttpPost]
+        public IActionResult Add(Category category)
+        {
+            using var context = new UdemyWebApiContext();
+            context.Categories.Add(category);
+            context.SaveChanges();
+
+            return Created("", category);
+        }
     }
 }

e39d09f1fd7a54fc3e1d12a3fb444905e7849907
Arif Damar <38383027+Arifdamar@users.noreply.github.com>
2020-10-01 14:17:19 +0300

    add repositories

---
+++ JWTAuthentication/Arif.JWTAuthentication.DataAccess/Concrete/EntityFrameworkCore/Repositories/EfGenericRepository.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Arif.JWTAuthentication.DataAccess.Concrete.EntityFrameworkCore.Context;
+using Arif.JWTAuthentication.DataAccess.Interfaces;
+using Arif.JWTAuthentication.Entities.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Arif.JWTAuthentication.DataAccess.Concrete.EntityFrameworkCore.Repositories
+{
+    public class EfGenericRepository<TEntity> : IGenericDal<TEntity> where TEntity : class, ITable, new()
+    {
+
+        public async Task<List<TEntity>> GetAll()
+        {
+            await using var context = new JWTAuthContext();
+
+            return await context.Set<TEntity>().ToListAsync();
+        }
+
+        public async Task<List<TEntity>> GetAllByFilter(Expression<Func<TEntity, bool>> filter)
+        {
+            await using var context = new JWTAuthContext();
+
+            return await context.Set<TEntity>().Where(filter).ToListAsync();
+        }
+
+        public async Task<TEntity> GetById(int id)
+        {
+            await using var context = new JWTAuthContext();
+
+            return await context.Set<TEntity>().FindAsync(id);
+        }
+
+        public async Task<TEntity> GetByFilter(Expression<Func<TEntity, bool>> filter)
+        {
+            await using var context = new JWTAuthContext();
+
+            return await context.Set<TEntity>().FirstOrDefaultAsync(filter);
+        }
+
+        public async Task Add(TEntity entity)
+        {
+            await using var context = new JWTAuthContext();
+            context.Add(entity);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task Update(TEntity entity)
+        {
+            await using var context = new JWTAuthContext();
+            context.Update(entity);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task Remove(TEntity entity)
+        {
+            await using var context = new JWTAuthContext();
+            context.Remove(entity);
+            await context.SaveChangesAsync();
+        }
+    }
+}
---
+++ JWTAuthentication/Arif.JWTAuthentication.DataAccess/Concrete/EntityFrameworkCore/Repositories/EfProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Arif.JWTAuthentication.DataAccess.Interfaces;
+using Arif.JWTAuthentication.Entities.Concrete;
+
+namespace Arif.JWTAuthentication.DataAccess.Concrete.EntityFrameworkCore.Repositories
+{
+    public class EfProductRepository : EfGenericRepository<Product>, IProductDal
+    {
+    }
+}

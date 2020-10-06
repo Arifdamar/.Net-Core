c37ceae59fcd008ac1da72d3def61b7a63b47459
Arif Damar <38383027+Arifdamar@users.noreply.github.com>
2020-10-06 13:58:27 +0300

    inversion of control

---
+++ JWTAuthentication/Arif.JWTAuthentication.Business/DependencyResolvers/MicrosoftIoc/CustomExtension.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Arif.JWTAuthentication.Business.Concrete;
+using Arif.JWTAuthentication.Business.Interfaces;
+using Arif.JWTAuthentication.DataAccess.Concrete.EntityFrameworkCore.Repositories;
+using Arif.JWTAuthentication.DataAccess.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Arif.JWTAuthentication.Business.DependencyResolvers.MicrosoftIoc
+{
+    public static class CustomExtension
+    {
+        public static void AddDependencies(this IServiceCollection services)
+        {
+            services.AddScoped(typeof(IGenericService<>), typeof(GenericManager<>));
+            services.AddScoped(typeof(IGenericDal<>), typeof(EfGenericRepository<>));
+
+            services.AddScoped<IProductDal, EfProductRepository>();
+            services.AddScoped<IProductService, ProductManager>();
+
+            services.AddScoped<IAppUserDal, EfAppUserRepository>();
+            services.AddScoped<IAppUserService, AppUserManager>();
+
+            services.AddScoped<IAppRoleDal, EfAppRoleRepository>();
+            services.AddScoped<IAppRoleService, AppRoleManager>();
+
+            services.AddScoped<IAppUserRoleDal, EfAppUserRoleRepository>();
+            services.AddScoped<IAppUserRoleService, AppUserRoleManager>();
+        }
+    }
+}
---
+++ JWTAuthentication/Arif.JWTAuthentication.WebApi/Controllers/ProductsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Arif.JWTAuthentication.Business.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Arif.JWTAuthentication.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var products = await _productService.GetAllAsync();
+
+            return Ok(products);
+        }
+    }
+}

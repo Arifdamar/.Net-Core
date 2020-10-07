6edb25f95653b9766cde548e926c2e7606142d7c
Arif Damar <38383027+Arifdamar@users.noreply.github.com>
2020-10-07 14:00:49 +0300

    add product controller's basic crud operations

--- JWTAuthentication/Arif.JWTAuthentication.WebApi/Controllers/ProductsController.cs
+++ JWTAuthentication/Arif.JWTAuthentication.WebApi/Controllers/ProductsController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Arif.JWTAuthentication.Business.Interfaces;
+using Arif.JWTAuthentication.Entities.Concrete;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -25,6 +26,43 @@ namespace Arif.JWTAuthentication.WebApi.Controllers
             var products = await _productService.GetAllAsync();
 
             return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var product = await _productService.GetByIdAsync(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(product);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(Product product)
+        {
+            await _productService.AddAsync(product);
+
+            return Created("", product);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(Product product)
+        {
+            await _productService.UpdateAsync(product);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _productService.RemoveAsync(await _productService.GetByIdAsync(id));
+
+            return NoContent();
         }
     }
 }

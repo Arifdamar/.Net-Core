34d1d6220070dabc41b408aa3a559e32ad039374
Qurricane <arif660damar@gmail.com>
2020-12-17 14:47:08 +0300

    Create project

---
+++ UdemyAzureStorage/AzureStorageLibrary/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Azure.Cosmos.Table;
+
+namespace AzureStorageLibrary.Models
+{
+    public class Product : TableEntity
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Stock { get; set; }
+        public string Color { get; set; }
+    }
+}
